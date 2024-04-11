02003d83e249e72afe3dd8e1c81fd6be3158478f
Caio Machado <machadocaio1403@gmail.com>
2024-04-11 20:15:19 -0300

    post

--- itemEmprestado.java
+++ itemEmprestado.java
@@ -1,2 +1,13 @@
 public class itemEmprestado {
+    private String dataDevolucao;
+    
+    //métodos getters e setters
+    public String getDataDevolucao(){
+        return dataDevolucao;
+    }
+    public void set(String dataDevolucao){
+        this.dataDevolucao = dataDevolucao;
+    }
+
+
 }
