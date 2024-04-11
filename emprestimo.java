c8b2f11fe99a4acf1deac090e66932a81316250e
Deagleoh <vralnelas1@gmai.com>
2024-04-11 19:55:54 -0300

    envio

--- emprestimo.java
+++ emprestimo.java
@@ -1,2 +1,28 @@
-public class emprestimo {
+public class Emprestimo {
+    private ArrayList<ItemEmprestimo> livros;
+    private String dataDevolucao;
+
+    public Emprestimo() {
+        livros = new ArrayList<ItemEmprestimo>();
+    }
+
+    public String getDataDevolucao() {
+        return dataDevolucao != null ? dataDevolucao : "";
+    }
+
+    public void setDataDevolucao(String data) {
+        this.dataDevolucao = data;
+    }
+
+    public ArrayList<ItemEmprestimo> getLivros() {
+        return livros;
+    }
+
+    public void setLivros(ArrayList<ItemEmprestimo> livros) {
+        this.livros = livros;
+    }
+
+    public void addLivro(ItemEmprestimo livro) {
+        livros.add(livro);
+    }
 }
