b7f7a95b02e12c43eb8d7c17a202b3db10ac593a
pedro <pedro.salles@alunos.unis.edu.br>
2024-04-11 19:23:23 -0300

    att

--- usuario.java
+++ usuario.java
@@ -1,2 +1,25 @@
 public class usuario {
+    private String login;
+    private String senha;
+
+    public usuario(String senha, String login) {
+        this.senha = senha;
+        this.login = login;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
 }
