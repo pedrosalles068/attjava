7a23a333363501630b96658d089b6095334d0c8b
pedro <pedro.salles@alunos.unis.edu.br>
2024-04-11 19:21:47 -0300

    att

--- autor.java
+++ autor.java
@@ -1,2 +1,16 @@
 public class autor {
+    private String nome;
+
+    public autor(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
+
