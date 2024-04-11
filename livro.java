dd9874c848430f0912ded88c216ae585ac73a31f
Miguel <miguelcunha1205@gmail.com>
2024-04-11 19:52:09 -0300

    FInal

--- livro.java
+++ livro.java
@@ -1,2 +1,23 @@
 public class livro {
+    private String lsbn;
+    private String titulo;
+
+    public livro (String registro, String autor){
+        this.lsbn=lsbn;
+        this.titulo=titulo;
+    }
+    public String getIsbn() {
+        return isbn;
+    }
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+    public String getTitulo() {
+        return titulo;
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+
+    }
+
 }
